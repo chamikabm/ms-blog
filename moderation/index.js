330aee769620d2a1362ec4e18623ca67e09162a6
chamikabm <chamika@bitsmedia.com>
2020-05-01 09:12:15 +0530

    Moderation service added.

---
+++ moderation/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  console.log('Received Event. Event : ', type);
+
+  if(type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id,
+        content,
+        postId,
+        status,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log('Listening on Port 4003');
+});
+

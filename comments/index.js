d04b3f62d3cef80549f9349f7d74f7efc86aa405
chamikabm <chamika@bitsmedia.com>
2020-05-01 10:15:39 +0530

    Moderation realted changes added.

--- comments/index.js
+++ comments/index.js
@@ -34,9 +34,28 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-  console.log('Received Event. Event : ', req.body.type);
-  res.send({})
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  console.log('Received Event. Event : ', type);
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => comment.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        content,
+        status,
+      },
+    })
+  }
+
+  res.send({});
 });
 
 app.listen(4001, () => {
